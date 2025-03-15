13c939e28223daa8c7e8251d603fd22d70a1f91b
Kabir Roshan <82226971+KabirRoshan@users.noreply.github.com>
2025-03-15 16:14:35 +0000

    Create sockPairs.js

---
+++ sockPairs.js
@@ -0,0 +1,67 @@
+// https://www.hackerrank.com/challenges/sock-merchant/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+function sockMerchant(n, ar) {
+    const sockMap = new Map();
+    let pairCount = 0;
+    
+    for (const sock of ar) {
+        if (sockMap.has(sock)) {
+            sockMap.set(sock, sockMap.get(sock) + 1);
+        } else {
+            sockMap.set(sock, 1);
+        }
+    }
+    
+    for (const count of sockMap.values()) {
+        pairCount += Math.floor(count / 2);
+    }
+    
+    return pairCount;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const ar = readLine().replace(/\s+$/g, '').split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    const result = sockMerchant(n, ar);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
